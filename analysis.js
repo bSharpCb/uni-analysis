3f966dfba17cb837cd5ff553b7a13517eaec073c
Brad Shffer <bradleyshaffer@Brads-MacBook-Pro.local>
2022-12-07 14:27:48 -0500

    update comments

--- analysis.js
+++ analysis.js
@@ -8,20 +8,26 @@ let csvArr = [];
 
 fs.createReadStream("./data/users.csv")
     .pipe(parse({ delimiter: ",", from_line: 2}))
+    // populate users object with {userID:gender} from the list of users provided by client 
     .on("data", function (row) {
         users[row[0]] = row[4];
     })
     .on("end", function () {
+        // parse events file, checking to see IDs match those in users{} 
         fs.createReadStream("./data/interactions.csv")
             .pipe(parse({ delimiter: ",", from_line: 2}))
             .on("data", function (row) {
+                // asset_ID
                 const aid = row[1];
                 if (!assets[aid]) {
+                    // create entry in assets{} if doesn't exist yet
                     assets[aid] = {"male": 0, "female": 0, "unknownUsers": 0};
                 }
                 if (users[row[0]]!=1 && users[row[0]]!=0) {
+                    // if there's no matching id in users{}, count event as an unknown user for that asset
                     assets[aid].unknownUsers += 1;
                 }else{
+                    // otherwise, lookup gender and increment in assets{}
                     if (users[row[0]] == 1) {
                         assets[aid].female += 1
                     }else{
@@ -35,7 +41,7 @@ fs.createReadStream("./data/users.csv")
                     const sumKnown = assets[id].male + assets[id].female;
                     const perMale = assets[id].male/sumKnown * 100;
                     const perFemale = assets[id].female/sumKnown * 100;
-                    const kindaSus = Math.abs(perMale - perFemale) < 10 ? 1 : 0;
+                    const kindaSus = Math.abs(perMale - perFemale);
                     csvArr.push({asset_id: id, male: assets[id].male, female: assets[id].female, unknownUsers: assets[id].unknownUsers, percentMale:`${Math.round(perMale)}%`, percentFemale: `${Math.round(perFemale)}%`, near_even: kindaSus, total_interactions: sumEvents});
                 }
                 const sassets = JSON.stringify(csvArr);
